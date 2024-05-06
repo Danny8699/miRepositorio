608a525f392b6adb508ef04dfef61377e88857b7
alumnoT <alumnoT@INF1-03>
2024-05-06 19:59:30 +0300

    Soy un maestro mental

--- MiProyectoEnGitHub/src/Identificacion/Identificador.java
+++ MiProyectoEnGitHub/src/Identificacion/Identificador.java
@@ -4,8 +4,10 @@ public class Identificador {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello world");
+		System.out.println("Soy un maestro de la mente");
 
+		
+		System.out.println("Se que no quieres estar aqui.");
 	}
 
 }
